c9cb0a1a06aadb528909c9ac76f4197aa3af1b5f
Pavel Paleček <palecek.pavel@gmail.com>
2017-11-21 22:24:38 +0100

    SFX build improvement

--- bundle.js
+++ bundle.js
@@ -2,23 +2,21 @@ var jspm = require('jspm')
 
 var builder = new jspm.Builder()
 
-var config = {
-  format: 'cjs',
-  minify: true
-}
-
-builder
-  .buildStatic('./src/moda/index.js', './public/dist/moda.sfx.js', config)
-  .then(function () {
-    console.log('SystemJS SFX build complete for "moda.js"');
-  }).catch(function (err) {
-    console.log(err);
-  })
+function sfxBuild(moduleName) {
+  var config = {
+    format: 'cjs',
+    minify: true
+  }
+  var src = './src/' + moduleName + '/index.js'
+  var dest = './public/dist/' + moduleName + '.sfx.js'
 
   builder
-  .buildStatic('./src/modb/index.js', './public/dist/modb.sfx.js', config)
-  .then(function () {
-    console.log('SystemJS SFX build complete for "modb.js"');
-  }).catch(function (err) {
-    console.log(err);
-  })
+    .buildStatic(src, dest, config)
+    .then(function () {
+      console.log('SystemJS SFX build complete for "' + moduleName + '.js"');
+    }).catch(function (err) {
+      console.log(err);
+    })
+}
+
+['moda', 'modb'].map(sfxBuild)
